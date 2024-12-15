fc8e7aaa27b2d81d062c590f5ac814f39cb9ff0c
Pug Kong <pugkong@tuta.io>
2024-12-15 20:12:53 +0300

    Cache Habitica responses

--- src/Habitica/Habitica.php
+++ src/Habitica/Habitica.php
@@ -6,6 +6,7 @@ namespace App\Habitica;
 
 use App\Http\Http;
 use DateInterval;
+use Psr\Cache\CacheItemInterface;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Lock\LockFactory;
@@ -19,10 +20,14 @@ use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
+use Symfony\Contracts\Cache\CacheInterface;
 
 final readonly class Habitica
 {
-    public function __construct(private Http $http)
+    private const string CACHE_TASKS = 'tasks';
+    private const string CACHE_TAGS = 'tags';
+
+    public function __construct(private Http $http, private CacheInterface $cache)
     {
     }
 
@@ -58,83 +63,123 @@ final readonly class Habitica
             lock: $lockFactory->createLock('api'),
         );
 
-        return new self(new Http($client, $serializer, $rateLimiter));
+        return new self(
+            http: new Http($client, $serializer, $rateLimiter),
+            cache: new FilesystemAdapter(directory: $cacheDir.'/cache'),
+        );
     }
 
     public function createTask(Task\Create\Request $request): Task\Create\Response
     {
-        return $this->http
-            ->post('api/v3/tasks/user')
-            ->status(201)
-            ->bodyJson($request)
-            ->fetchJson(Task\Create\Response::class)
-        ;
+        try {
+            return $this->http
+                ->post('api/v3/tasks/user')
+                ->status(201)
+                ->bodyJson($request)
+                ->fetchJson(Task\Create\Response::class)
+            ;
+        } finally {
+            $this->cache->delete(self::CACHE_TASKS);
+        }
     }
 
     public function deleteTask(string $id): void
     {
-        $this->http
-            ->delete('api/v3/tasks/{id}', ['id' => $id])
-            ->fetch()
-        ;
+        try {
+            $this->http
+                ->delete('api/v3/tasks/{id}', ['id' => $id])
+                ->fetch()
+            ;
+        } finally {
+            $this->cache->delete(self::CACHE_TASKS);
+        }
     }
 
     public function listTasks(): Task\List\Response
     {
-        return $this->http
-            ->get('api/v3/tasks/user')
-            ->fetchJson(Task\List\Response::class)
-        ;
+        return $this->cache->get(self::CACHE_TASKS, function (CacheItemInterface $item): Task\List\Response {
+            $item->expiresAfter(60 * 60);
+
+            return $this->http
+                ->get('api/v3/tasks/user')
+                ->fetchJson(Task\List\Response::class)
+            ;
+        });
     }
 
     public function scoreTask(string $id, Task\ScoreDirection $direction): void
     {
-        $this->http
-            ->post('api/v3/tasks/{id}/score/{direction}', ['id' => $id, 'direction' => $direction->value])
-            ->fetch()
-        ;
+        try {
+            $this->http
+                ->post('api/v3/tasks/{id}/score/{direction}', ['id' => $id, 'direction' => $direction->value])
+                ->fetch()
+            ;
+        } finally {
+            $this->cache->delete(self::CACHE_TASKS);
+        }
     }
 
     public function addTagToTask(string $taskId, string $tagId): void
     {
-        $this->http
-            ->post('api/v3/tasks/{taskId}/tags/{tagId}', ['taskId' => $taskId, 'tagId' => $tagId])
-            ->fetch()
-        ;
+        try {
+            $this->http
+                ->post('api/v3/tasks/{taskId}/tags/{tagId}', ['taskId' => $taskId, 'tagId' => $tagId])
+                ->fetch()
+            ;
+        } finally {
+            $this->cache->delete(self::CACHE_TASKS);
+        }
     }
 
     public function deleteTagFromTask(string $taskId, string $tagId): void
     {
-        $this->http
-            ->delete('api/v3/tasks/{taskId}/tags/{tagId}', ['taskId' => $taskId, 'tagId' => $tagId])
-            ->fetch()
-        ;
+        try {
+            $this->http
+                ->delete('api/v3/tasks/{taskId}/tags/{tagId}', ['taskId' => $taskId, 'tagId' => $tagId])
+                ->fetch()
+            ;
+        } finally {
+            $this->cache->delete(self::CACHE_TASKS);
+        }
     }
 
     public function createTag(Tag\Create\Request $request): Tag\Create\Response
     {
-        return $this->http
-            ->post('api/v3/tags')
-            ->status(201)
-            ->bodyJson($request)
-            ->fetchJson(Tag\Create\Response::class)
-        ;
+        try {
+            return $this->http
+                ->post('api/v3/tags')
+                ->status(201)
+                ->bodyJson($request)
+                ->fetchJson(Tag\Create\Response::class)
+            ;
+        } finally {
+            $this->cache->delete(self::CACHE_TAGS);
+        }
     }
 
     public function deleteTag(string $id): void
     {
-        $this->http
-            ->delete('api/v3/tags/{id}', ['id' => $id])
-            ->fetch()
-        ;
+        try {
+            $this->http
+                ->delete('api/v3/tags/{id}', ['id' => $id])
+                ->fetch()
+            ;
+        } finally {
+            $this->cache->delete(self::CACHE_TAGS);
+            $this->cache->delete(self::CACHE_TASKS);
+        }
     }
 
     public function listTags(): Tag\List\Response
     {
-        return $this->http
-            ->get('api/v3/tags')
-            ->fetchJson(Tag\List\Response::class)
-        ;
+        return $this->cache->get(self::CACHE_TAGS, function (CacheItemInterface $item): Tag\List\Response {
+            $item->expiresAfter(1 * 60 * 60);
+
+            return $this->http
+                ->get('api/v3/tags')
+                ->fetchJson(Tag\List\Response::class)
+            ;
+        });
     }
 
     public function getUser(): User\Get\Response
